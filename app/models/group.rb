f1128e4bc43c52ba5f05fbe0d558d4d59169d526
sarazaToussaint <tmsaraza@gmail.com>
2022-09-12 15:47:49 +0200

    Add validation and association to the Group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,9 @@
 class Group < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, class_name: 'User', foreign_key: :user_id
+  has_many :group_entities
+  has_many :entities, through: :group_entities
+
+
+  validates :name, presence: true, length: { 100 }
+  validates :icon, presence: true, length: { 100 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :groups, foreign_key: :user_id
-  has_many :entities, foreign_key: :user_id 
-  
+  has_many :groups, class_name: 'Group', foreign_key: :user_id
+  has_many :entities, class_name: 'Entity', foreign_key: :user_id 
+
   validation :name, presence: true, length: { maximum: 100 }    
 end
